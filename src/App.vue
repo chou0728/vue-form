87d4df46f9d40ec70453d5f7dbe586feacea4564
Eric Chou <eric.chou@cloud-interactive.com>
2019-02-17 02:49:52 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,86 @@
 <template>
   <div id="app">
+    <h1>Vue Form</h1>
     <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <!-- <Form/> -->
+    <FormWithItems/>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import Form from './components/Form.vue';
+import FormWithItems from './components/FormWithItems';
 
 export default {
   name: "app",
   components: {
-    HelloWorld
+    Form,
+    FormWithItems
   }
 };
 </script>
 
 <style>
 #app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  text-align: center;
+}
+
+body {
+  margin: 0;
+  padding: 0;
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen",
+    "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue",
+    sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+
+code {
+  font-family: source-code-pro, Menlo, Monaco, Consolas, "Courier New", monospace;
+}
+
+.form-item {
+  display: flex;
+  justify-content: space-between;
+  width: 600px;
+  padding: 20px 0;
+  margin: 0 auto;
+}
+
+.form-item__input {
+  width: 500px;
+  height: 30px;
+  border-radius: 5px;
+  font-size: 20px;
+  padding: 5px 20px;
+}
+
+
+label {
+  width: 40px;
+  text-align: left;
+  padding-right: 20px;
+  line-height: 44px;
+}
+
+.form-item__submit {
+  display: block;
+  margin: 0 auto;
+  width: 200px;
+  height: 40px;
+  -webkit-appearance: none;
+}
+
+.show-area {
+  text-align: left;
+  width: 500px;
+  margin: 20px auto;
+  padding: 20px 50px;
+  border: 2px solid #eee;
+}
+
+img {
+  width: 100px;
+  padding-bottom: 10px;
 }
 </style>
---
+++ src/components/Form.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="example-form">
+    <div class="form-item">
+      <label>姓名</label>
+      <input type="text" class="form-item__input" v-model="userName"/>
+    </div>
+    <div class="form-item">
+      <label>電話</label>
+      <input type="text" class="form-item__input" v-model="phone"/>
+    </div>
+    <div class="form-item">
+      <label>地址</label>
+      <input type="text" class="form-item__input" v-model="address"/>
+    </div>
+    <button class="form-item__submit" @click="handleSubmit">Submit</button>
+    <div class="show-area">
+      <p>姓名: {{userName}}</p>
+      <p>電話: {{phone}}</p>
+      <p>地址: {{address}}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+const initialState = () => { 
+  return JSON.parse(
+    JSON.stringify({
+      userName: '',
+      phone: '',
+      address: ''
+    })
+  )
+};
+
+export default {
+  name: "Form",
+  props: {
+  },
+  data() {
+    return initialState()
+  },
+  methods: {
+    handleSubmit(event) {
+      event.preventDefault();
+      alert(`您的表單已送出: 姓名:${this.userName} 電話:${this.phone} 住址:${this.address}`);
+      Object.assign(this.$data, initialState());
+    }
+  }
+};
+// JSON.parse(JSON.stringify(Object))
+// this.$data = initialState() 會報錯  [Vue警告]：避免替换提體根$data。請改用鑲嵌數據属性
+</script>
+
